27b98298eb4546dc90941641b811bd6e0e2481bc
프리마 <fc1943s@pm.me>
2023-08-21 04:25:06 -0300

    tests

--- The Spiral Language 2/CodegenUtils.fs
+++ The Spiral Language 2/CodegenUtils.fs
@@ -11,7 +11,7 @@ type CodegenEnv =
 
 let line x (s : string) = x.text.Append(' ', x.indent).AppendLine s |> ignore
 let indent x = {x with indent=x.indent+4}
-let add_dec_point (x : string) = if x.IndexOf('.') = -1 then x + ".0" else x
+let add_dec_point (x : string) = if x.IndexOf('.') = -1 && x.Contains "E" |> not then x + ".0" else x
 
 exception CodegenError of PartEval.Prepass.Range option * string
 exception CodegenErrorWithPos of Trace * string
